bac7829ef9dbd62ffaf8ef93e552f2984ac81f56
Alexandr Sergeev <rboby.box@mail.ru>
2020-03-21 16:33:21 +0300

    Constructor, index-operator and Length method

---
+++ IndexerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IndexerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("IndexerTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0bb24013-6754-4310-a3ff-3ca94281217a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Task1/Indexer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Indexer
+    {
+        double[] array;
+        int firstIndex, lastIndex;
+        public Indexer(double[] array, int firstIndex, int length)
+        {
+            this.array = array;
+            this.firstIndex = firstIndex;
+            this.lastIndex = firstIndex + length;
+        }
+        public int Length
+        {
+            get => this.lastIndex - this.firstIndex;
+        }
+        public double this[int index]
+        {
+            get
+            {
+                return this.array[index + firstIndex];
+            }
+            set
+            {
+                this.array[index] = value;
+            }
+        }
+
+    }
+}
