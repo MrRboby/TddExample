a23c0ec95ad614c12e9a9d362dcf8455a1d10a2c
Alexandr Sergeev <rboby.box@mail.ru>
2020-03-22 12:42:37 +0300

    Invalid argument exception cpp added

--- Task1Cpp/Indexer.cpp
+++ Task1Cpp/Indexer.cpp
@@ -2,6 +2,8 @@
 #include <stdexcept>
 
 Indexer::Indexer(double* array, int arrayLength, int firstIndex, int length) {
+	if (firstIndex < 0 || length <= 0 || firstIndex + length >= arrayLength)
+		throw std::invalid_argument("Incorrect first index or length");
 	this->array = array;
 	this->firstIndex = firstIndex;
 	this->lastIndex = firstIndex + length - 1;
